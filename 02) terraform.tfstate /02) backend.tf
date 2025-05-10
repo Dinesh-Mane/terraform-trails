186095d032ee15ffbbe19030850285abb3183994
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-10 10:52:31 +0530

    Update 02) backend.tf

--- 02) terraform.tfstate /02) backend.tf
+++ 02) terraform.tfstate /02) backend.tf
@@ -1,1 +1,11 @@
+# Terraform backend configuration for storing the state file in an S3 bucket and using DynamoDB for state locking, specifically for the prod environment and the app component.
 
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-states"
+    key            = "prod/app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
